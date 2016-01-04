8d706af74f8990bde248bc08188851103e9450a4
shaunkawano <shaunkawano@gmail.com>
2016-01-04 22:39:09 +0900

    Modify to format

--- data-binding-sample/src/main/java/com/shoheiskawano/databindingsample/MainActivity.java
+++ data-binding-sample/src/main/java/com/shoheiskawano/databindingsample/MainActivity.java
@@ -3,21 +3,19 @@ package com.shoheiskawano.databindingsample;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import com.shoheiskawano.databindingsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        // binding data with the layout
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        User user = new User("Test", "User");
-        binding.setUser(user);
+    // binding data with the layout
+    ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    User user = new User("Test", "User");
+    binding.setUser(user);
 
-        // Notice here that we do not have any Activity#setContentView method; we have already done the same thing
-        // by calling DataBindingUtil#setContentView above.
-    }
+    // Notice here that we do not have any Activity#setContentView method; we have already done the same thing
+    // by calling DataBindingUtil#setContentView above.
+  }
 }
